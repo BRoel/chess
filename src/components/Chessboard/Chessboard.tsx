bdc792b1cd55d20b54f89a684f08164dd648f4c0
Brennan Roel <brennanroel@gmail.com>
2021-08-23 13:33:16 -0500

    added pieces to board

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,25 +1,64 @@
 import Tile from '../Tile/Tile';
 import './Chessboard.css';
 
+const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8']
+
+interface Piece {
+    image: string;
+    x: number;
+    y: number;
+}
+
+const pieces: Piece[] = [];
+
+for (let i = 0; i < 8; i++) {
+    pieces.push({ image: 'images/pawn_b.png', x: i, y: 6 });
+}
+
+for (let i = 0; i < 8; i++) {
+    pieces.push({ image: 'images/pawn_w.png', x: i, y: 1 });
+}
+
+for (let p = 0; p < 2; p++) {
+    const type = p === 0 ? 'b' : 'w';
+    const y = p === 0 ? 7 : 0;
+
+    pieces.push({ image: `images/rook_${type}.png`, x: 0, y });
+    pieces.push({ image: `images/rook_${type}.png`, x: 7, y });
+
+    pieces.push({ image: `images/knight_${type}.png`, x: 1, y });
+    pieces.push({ image: `images/knight_${type}.png`, x: 6, y });
+
+    pieces.push({ image: `images/bishop_${type}.png`, x: 2, y });
+    pieces.push({ image: `images/bishop_${type}.png`, x: 5, y });
+
+    pieces.push({ image: `images/queen_${type}.png`, x: 3, y });
+
+    pieces.push({ image: `images/king_${type}.png`, x: 4, y });
+
+}
+
 export default function Chessboard() {
     
-    const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8']
+
 
     const board = []
 
     for (let i = verticalAxis.length-1; i >= 0; i--) {
         for (let j = 0; j < horizontalAxis.length; j++) {
             const number = i+j+2
+            let image = undefined;
 
-            board.push(<Tile number={number}/>)
+            pieces.forEach((p) => {
+                if (p.y === i && p.x === j) {
+                    image = p.image;
+                }
+            });
 
+            board.push(<Tile image={image} number={number} />)
         }
     }
     
-    return (
-        <div id='chessboard'>
-            {board}
-        </div>
-    )
+    return <div id='chessboard'>{board}</div>
 }
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,13 +1,23 @@
 import './Tile.css';
 
 interface Props {  // learn interface Props
+    image?: string;
     number: number;
 }
 
-export default function Tile({ number }: Props) {
+export default function Tile({ number, image }: Props) {
     if (number % 2 === 0) {
-        return <div className='tile tile-black'></div>;
+        return (
+            <div className='tile tile-black'>
+                <img src={image} />
+            </div>
+        );
     } else {
-        return <div className='tile tile-white'></div>;
+        return (
+            <div className='tile tile-white'>
+                {' '}
+                <img src={image} />
+            </div>
+        );
     }
 }
